b54485d14b4dfa2d6654838a672a0cc482a6d430
nimishat <nimishathakkar83@gmail.com>
2018-07-13 15:36:06 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -6,17 +6,17 @@ def initialize(name)
 end
   
   def flood
-    @discharge = discharge + 1.3
+    @discharge = discharge * 1.3
   end
   
   def dry_up
-    @discharge = discharge - 1.5
+    @discharge = discharge * 1.5
   end
   
 end
-rivers1 = River.new = ("The Nile")
-rivers2 = River.new = ("The Mississippi")
-rivers3 = River.new = ("The Amazon")
-rivers4 = River.new = ("The Seine")
-rivers5 = River.new = ("The Yangtze")
-rivers6 = River.new = ("The Euphrates")
+# rivers1 = River.new = ("The Nile")
+# rivers2 = River.new = ("The Mississippi")
+# rivers3 = River.new = ("The Amazon")
+# rivers4 = River.new = ("The Seine")
+# rivers5 = River.new = ("The Yangtze")
+# rivers6 = River.new = ("The Euphrates")
