7bc18cac179b5f5872fcdf2f3cafb6fdb4b0e58b
nimishat <nimishathakkar83@gmail.com>
2018-07-13 15:19:53 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,1 +1,16 @@
-
+class River
+  attr_accessor :length, :countries, :discharge
+  
+  
+  def flood(discharge)
+    discharge + 1.3
+  end
+  
+  def dry_up
+    discharge - 1.5
+  end
+  
+  
+  
+  
+end
