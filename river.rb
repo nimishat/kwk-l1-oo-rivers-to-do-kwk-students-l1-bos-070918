38474a3587c2da257b688ff00969799aae14298b
nimishat <nimishathakkar83@gmail.com>
2018-07-13 15:27:46 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,7 +1,9 @@
 class River
   attr_accessor :name, :length, :countries, :discharge
   
-  
+def initialize
+  @name = name
+end
   
   def flood
     @discharge = discharge + 1.3
@@ -11,5 +13,5 @@ class River
     @discharge = discharge - 1.5
   end
   
-  
 end
+river = River.new = "The Nile"
