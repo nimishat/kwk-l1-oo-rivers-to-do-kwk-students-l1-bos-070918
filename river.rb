0f899639afae9b73555230989d2ee39057229a7f
nimishat <nimishathakkar83@gmail.com>
2018-07-13 15:36:55 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -14,9 +14,9 @@ end
   end
   
 end
-# rivers1 = River.new = ("The Nile")
-# rivers2 = River.new = ("The Mississippi")
-# rivers3 = River.new = ("The Amazon")
-# rivers4 = River.new = ("The Seine")
-# rivers5 = River.new = ("The Yangtze")
-# rivers6 = River.new = ("The Euphrates")
+rivers1 = River.new = ("The Nile")
+rivers2 = River.new = ("The Mississippi")
+rivers3 = River.new = ("The Amazon")
+rivers4 = River.new = ("The Seine")
+rivers5 = River.new = ("The Yangtze")
+rivers6 = River.new = ("The Euphrates")
